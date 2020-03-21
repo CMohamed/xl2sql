66499d1dc17368ff9202988d1d37e562d0ce84f8
mohamed.chadad <mohamed.chadad@nimblways.com>
2020-03-21 22:09:17 +0100

    handle date

--- xlReader.js
+++ xlReader.js
@@ -3,18 +3,24 @@ const readXlsxFile = require('read-excel-file/node');
 
 
 let sqlData = '';
+const formatDate = date => l = date.toLocaleDateString().split('/').reverse().join('-');
 const getColumns = columns => columns.map(e => "'" + e + "'").join(",");
 const getValues = row => row.map(e => {
-    if (typeof(e) === 'number') {
+    console.log("e ")
+    console.log(e, typeof(e));
+    console.log("e ")
+    if (e === null) {
+        return '';
+    } else if (typeof(e) === 'number') {
         return e;
-    } else {
+    } else if (typeof(e) === 'string') {
         return e ? "'" + e + "'" : e ;
+    } else {
+        return "'" + formatDate(e) + "'";
     }
 }).join(",");
 const insertQuery = (table, columns, values) => `insert into ${table} (${getColumns(columns)}) values (${getValues(values)});`;
-readXlsxFile('appended.xlsx').then((rows) => {
-    // `rows` is an array of rows
-    // each row being an array of cells.
+readXlsxFile('Food2eme.xlsx').then((rows) => {
 
     const columns = rows[0];
     let line = '';
@@ -23,7 +29,7 @@ readXlsxFile('appended.xlsx').then((rows) => {
         line = insertQuery('Food', columns, rows[i]);
         sqlData += (line + "\n");
     }
-    fs.writeFile( 'appendedOutput.sql', sqlData, (err) => { 
+    fs.writeFile( 'FoodOutOutput.sql', sqlData, (err) => { 
       
         // In case of a error throw err. 
         if (err) throw err; 
