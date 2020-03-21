afad458f595c538d6aae598746c8e5eac55a51aa
mohamed.chadad <mohamed.chadad@nimblways.com>
2020-03-21 23:52:06 +0100

    final

--- xlReader.js
+++ xlReader.js
@@ -1,37 +1,39 @@
 const fs = require('fs');
 const readXlsxFile = require('read-excel-file/node');
 
-
-let sqlData = '';
 const formatDate = date => l = date.toLocaleDateString().split('/').reverse().join('-');
 const getColumns = columns => columns.map(e => "'" + e + "'").join(",");
 const getValues = row => row.map(e => {
-    console.log("e ")
-    console.log(e, typeof(e));
-    console.log("e ")
     if (e === null) {
         return '';
-    } else if (typeof(e) === 'number') {
+    } else if (typeof (e) === 'number') {
         return e;
-    } else if (typeof(e) === 'string') {
-        return e ? "'" + e + "'" : e ;
+    } else if (typeof (e) === 'string') {
+        return e ? "'" + e + "'" : e;
     } else {
         return "'" + formatDate(e) + "'";
     }
 }).join(",");
-const insertQuery = (table, columns, values) => `insert into ${table} (${getColumns(columns)}) values (${getValues(values)});`;
-readXlsxFile('Food2eme.xlsx').then((rows) => {
 
-    const columns = rows[0];
-    let line = '';
-    for (let i = 1; i < rows.length; i++) {
-        console.log(rows[i]);
-        line = insertQuery('Food', columns, rows[i]);
-        sqlData += (line + "\n");
-    }
-    fs.writeFile( 'FoodOutOutput.sql', sqlData, (err) => { 
-      
-        // In case of a error throw err. 
-        if (err) throw err; 
-    })
-});
+const insertQuery = (table, columns, values) => `insert into ${table} (${getColumns(columns)}) values (${getValues(values)});`;
+
+const xl2sql = (filename, tableName) => {
+    let sqlData = '';
+    readXlsxFile(`${filename}.xlsx`).then((rows) => {
+        const columns = rows[0];
+        let line = '';
+        for (let i = 1; i < rows.length; i++) {
+            console.log(rows[i]);
+            line = insertQuery(tableName, columns, rows[i]);
+            sqlData += (line + "\n");
+        }
+        fs.writeFile(`${filename}.sql`, sqlData, (err) => {
+            if (err) throw err;
+        })
+    });
+}
+
+xl2sql('Food2eme', 'foodsec');
+xl2sql('appended', 'appended');
+xl2sql('food_dataN3', 'food_data');
+xl2sql('fooddiction', 'food_diction');
